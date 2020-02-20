53b5aa5c4bd760a83066fda8c3cfe22786a66298
Albert (AJ) St. Aubin <astaubin@edx.org>
2020-02-20 16:20:40 -0500

    Test

--- openedx/core/djangoapps/external_user_ids/tests/test_signals.py
+++ openedx/core/djangoapps/external_user_ids/tests/test_signals.py
@@ -15,11 +15,6 @@ from openedx.core.djangoapps.external_user_ids.models import ExternalId, Externa
 from openedx.core.djangolib.testing.utils import CacheIsolationTestCase
 from xmodule.modulestore.tests.django_utils import ModuleStoreTestCase
 
-# Tests
-# Creating a new enrollment adds an external ID
-# A second enrollment in a Program course will not create a second external UUID
-# An enrollment in a course that is not part of a non-MB program will not create an external UUID
-
 
 class MicrobachelorsExternalIDTest(ModuleStoreTestCase, CacheIsolationTestCase):
     ENABLED_CACHES = ['default']
@@ -28,14 +23,12 @@ class MicrobachelorsExternalIDTest(ModuleStoreTestCase, CacheIsolationTestCase):
     def setUpClass(cls):
         super(MicrobachelorsExternalIDTest, cls).setUpClass()
 
+        cls.course_list = []
+        cls.user = UserFactory.create()
         cls.course_keys = [
             CourseKey.from_string('course-v1:edX+DemoX+Test_Course'),
             CourseKey.from_string('course-v1:edX+DemoX+Another_Test_Course'),
         ]
-        cls.course_list = []
-        cls.user = UserFactory.create()
-        for course_key in cls.course_keys:
-            cls.course_list.append(CourseFactory(id=course_key))
         ExternalIdType.objects.create(
             name=ExternalIdType.MICROBACHELORS_COACHING,
             description='test'
@@ -50,15 +43,20 @@ class MicrobachelorsExternalIDTest(ModuleStoreTestCase, CacheIsolationTestCase):
     def _create_cached_program(self):
         """ helper method to create a cached program """
         program = ProgramFactory.create()
-        course = program['courses'][0]['course_runs'][0]['key']
+
+        for course_key in self.course_keys:
+            program['courses'].append(CourseFactory(id=course_key))
+
         program['type'] = 'MicroBachelors'
         program['type_slug'] = 'microbachelors'
 
-        cache.set(
-            COURSE_PROGRAMS_CACHE_KEY_TPL.format(course_run_id=course),
-            [program['uuid']],
-            None
-        )
+        for course in program['courses']:
+            course_run = course['course_runs'][0]['key']
+            cache.set(
+                COURSE_PROGRAMS_CACHE_KEY_TPL.format(course_run_id=course_run),
+                [program['uuid']],
+                None
+            )
         cache.set(
             PROGRAM_CACHE_KEY_TPL.format(uuid=program['uuid']),
             program,
@@ -82,3 +80,37 @@ class MicrobachelorsExternalIDTest(ModuleStoreTestCase, CacheIsolationTestCase):
         )
         assert external_id is not None
         assert external_id.external_id_type.name == ExternalIdType.MICROBACHELORS_COACHING
+
+    def test_second_enroll_mb_no_new_external_id(self):
+        course_run_key1 = self.program['courses'][0]['course_runs'][0]['key']
+        course_run_key2 = self.program['courses'][1]['course_runs'][0]['key']
+
+        # Enroll user
+        CourseEnrollment.objects.create(
+            course_id=course_run_key1,
+            user=self.user,
+            mode=CourseMode.VERIFIED,
+        )
+        external_id = ExternalId.objects.get(
+            user=self.user
+        )
+        assert external_id is not None
+        assert external_id.external_id_type.name == ExternalIdType.MICROBACHELORS_COACHING
+        original_external_user_uuid = external_id.external_user_id
+
+        CourseEnrollment.objects.create(
+            course_id=course_run_key2,
+            user=self.user,
+            mode=CourseMode.VERIFIED,
+        )
+        enrollments = CourseEnrollment.objects.filter(user=self.user)
+
+        assert len(enrollments) == 2
+
+        external_ids = ExternalId.objects.filter(
+            user=self.user
+        )
+
+        assert len(external_ids) == 1
+        assert external_ids[0].external_id_type.name == ExternalIdType.MICROBACHELORS_COACHING
+        assert original_external_user_uuid == external_ids[0].external_user_id
